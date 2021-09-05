740edc7beaf90b057d6bf3154daf729cb46bf900
ka1peshc <kalpesh111997@gmail.com>
2021-09-05 15:29:23 +0530

    Program to give minimun number of notes as change for amount

---
+++ NUnitTesting/NUnitTesting/MinimumChangesNotes.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTesting
+{
+    class MinimumChangesNotes
+    {
+        internal void FindChanges(int amount)
+        {
+            int note=0;
+            int amt = amount;
+            while ( amount != 0 )
+            {
+                if(amount >= 1000)
+                {
+                    amount -= 1000;
+                    note++;
+                }
+                else if(amount >= 500)
+                {
+                    amount -= 500;
+                    note++;
+                }
+                else if (amount >= 100)
+                {
+                    amount -= 100;
+                    note++;
+                }
+                else if (amount >= 50)
+                {
+                    amount -= 50;
+                    note++;
+                }
+                else if (amount >= 10)
+                {
+                    amount -= 10;
+                    note++;
+                }
+                else if (amount >= 5)
+                {
+                    amount -= 5;
+                    note++;
+                }else if (amount >= 2)
+                {
+                    amount -= 2;
+                    note++;
+                }else if (amount >= 1)
+                {
+                    amount -= 1;
+                    note++;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }            }
+            Console.WriteLine("Number of notes required to give {0} is {1}", amt, note);
+        }
+    }
+}
--- NUnitTesting/NUnitTesting/Program.cs
+++ NUnitTesting/NUnitTesting/Program.cs
@@ -6,7 +6,11 @@ namespace NUnitTesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Enter amount to get change from vending machine");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            MinimumChangesNotes mcn = new MinimumChangesNotes();
+            mcn.FindChanges(amount);
         }
     }
 }
