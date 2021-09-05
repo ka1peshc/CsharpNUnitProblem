9b9f5f6375237aff663220329612ccc34b8cf5d7
ka1peshc <kalpesh111997@gmail.com>
2021-09-05 18:52:44 +0530

    converting temprature from celsius to fahrenheit visa versa

--- NUnitTesting/NUnitTesting/Program.cs
+++ NUnitTesting/NUnitTesting/Program.cs
@@ -12,7 +12,7 @@ namespace NUnitTesting
             int amount = Convert.ToInt32(Console.ReadLine());
             MinimumChangesNotes mcn = new MinimumChangesNotes();
             mcn.FindChanges(amount);
-            */
+            
             //day of the week
             Console.WriteLine("Enter month then day and then year respectivily");
             int m = Convert.ToInt32(Console.ReadLine());
@@ -21,6 +21,30 @@ namespace NUnitTesting
             Console.WriteLine("Entered values: Month is" + m + " Day is " + d + " year is" + y);
             DayOfWeek dow = new DayOfWeek();
             dow.checkForDayOfWeek(m, d, y);
+            */
+
+            //Temprature convertor
+            TempratureConverter tc = new TempratureConverter();
+            Console.WriteLine("Enter temprature in number");
+            int tempNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Select convertion");
+            Console.WriteLine("1. Celsius to Fahrenheit");
+            Console.WriteLine("2. Fahrenheit to Celsius");
+            int option = int.Parse(Console.ReadLine());
+            if (option == 1)
+            {
+                tc.CelsiusToFahrenheit(tempNumber);
+            }
+            else if (option == 2)
+            {
+                tc.FahrenheitToCelsius(tempNumber);
+            }
+            else
+            {
+                Console.WriteLine("Enter valid input option");
+            }
+
+
         }
     }
 }
---
+++ NUnitTesting/NUnitTesting/TempratureConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTesting
+{
+    class TempratureConverter
+    {
+        internal void CelsiusToFahrenheit(int temp)
+        {
+            //(°C × 9/5) + 32 = °F
+            int tempInF = temp * (9 / 5) + 32;
+            Console.WriteLine("{0} celsius into {1} fahrenheit", temp, tempInF);
+        }
+        internal void FahrenheitToCelsius(int temp)
+        {
+            //(°F − 32) x 5/9 = °C
+            int tempInF = (temp -32) * (5/9);
+            Console.WriteLine("{0} fahrenheit into {1} celsius", temp, tempInF);
+        }
+
+    }
+}
