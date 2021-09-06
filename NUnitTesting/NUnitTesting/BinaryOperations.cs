99ae53d56e533b72351b273358bda43880aea650
ka1peshc <kalpesh111997@gmail.com>
2021-09-06 13:58:08 +0530

    Display reverse number and its decimal form

--- NUnitTesting/NUnitTesting/BinaryOperations.cs
+++ NUnitTesting/NUnitTesting/BinaryOperations.cs
@@ -17,7 +17,7 @@ namespace NUnitTesting
                 binaryArr[i] = num % 2;
                 num = num / 2;
             }
-            for(int i = 0; i < binaryArr.Length; i++)
+            for (int i = 0; i < binaryArr.Length; i++)
             {
                 Console.Write(binaryArr[i]);
                 if (i < 4)
@@ -26,16 +26,33 @@ namespace NUnitTesting
                 }
                 else
                 {
-                    nibble2[i-4] = binaryArr[i];
+                    nibble2[i - 4] = binaryArr[i];
                 }
             }
             var myList = new List<int>();
-            myList.AddRange(nibble1);
             myList.AddRange(nibble2);
+            myList.AddRange(nibble1);
             reverseArr = myList.ToArray();
+            string addition = " ";
+            double temp=0;
+            double sum=0;
+            int count = 7;
+            Console.WriteLine("\n");
             for (int i = 0; i < reverseArr.Length; i++)
             {
                 Console.Write(reverseArr[i]);
+                if (reverseArr[i]==1)
+                {
+                    temp = Math.Pow(2, count);
+                    sum += temp;
+                }
+                addition = addition + temp.ToString()+"+";
+                count--;
             }
+            Console.WriteLine("\n");
+            addition = addition.Remove(addition.Length - 1, 1);
+            Console.WriteLine("reverse number is {0}", sum);
+            Console.WriteLine(addition);
+        }
     }
 }
